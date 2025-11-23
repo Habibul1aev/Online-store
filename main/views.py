d5f63dd6bfff712b9c26c329763bdaf54d58fec6
Habibul1aev <hhabibullaevfirdavs@gmail.com>
2025-11-23 09:59:10 +0600

    Home page & about us

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def main(request):
+    context = {
+        'title': "Главная"
+    }
+    return render(request, 'main/body.html', context)
+
+def about(request):
+    context = {
+        'title': "О нас"
+    }
+    return render(request, 'main/about.html', context)
--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from main import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main, name='main'),
+    path('about/', views.about, name='about'),
+
 ]
