73580b9904881c11c8cd658f25b1d3f2483448b3
Habibul1aev <hhabibullaevfirdavs@gmail.com>
2025-11-24 23:39:54 +0600

    templates catalog & product

--- main/views.py
+++ main/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 
 def main(request):
+
+
     context = {
         'title': "Главная"
     }
     return render(request, 'main/body.html', context)
 
 def about(request):
+
+
     context = {
         'title': "О нас"
     }
