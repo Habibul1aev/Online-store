96b6bb245e46198b7c3642b6f53087987fe61dda
Habibul1aev <hhabibullaevfirdavs@gmail.com>
2025-12-03 00:10:03 +0600

    The product filter on the site page and urlencode

--- goods/views.py
+++ goods/views.py
@@ -1,25 +1,44 @@
-from django.contrib.admin.templatetags.admin_list import pagination
+
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 
 from goods.models import Products, Categories
 
 
-def catalog(request, category_slug):
-    page = request.GET.get('page', 1)
+def catalog(request, category_slug = None):
+    category = get_object_or_404(Categories, slug = category_slug)
 
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
-        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+        goods = Products.objects.filter(category=category, is_publish=True)
 
-    paginator = Paginator(goods, 9)
+    min_price = request.GET.get('from_price', None)
+    max_price = request.GET.get('to_price', None)
+    order_by = request.GET.get('order_by', None)
+    on_sale = request.GET.get('on_sale', None)
+
+    if min_price and max_price:
+        if min_price <= max_price:
+            goods = goods.filter(price__range=(min_price, max_price))
+        else:
+            goods = goods.filter(price__range=(max_price, min_price))
+    elif min_price:
+        goods = goods.filter(price__gte = min_price)
+    elif max_price:
+        goods = goods.filter(price__lte = max_price)
+    if on_sale:
+        goods = goods.filter(discount__gt = 0)
+    if order_by:
+        goods = goods.order_by(order_by)
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
-
 
     context = {
         'title': 'Каталог',
-        'goods': current_page
+        'goods': current_page,
     }
     return render(request, 'goods/catalog.html', context)
 
