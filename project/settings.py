13b84a84ed3473b72c3597e0744dba98733cf822
Habibul1aev <hhabibullaevfirdavs@gmail.com>
2025-12-08 01:18:46 +0600

    Login form and controller are ready

--- project/settings.py
+++ project/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Home',
+        'NAME': 'home',
         'USER': 'Admin',
         'PASSWORD': '2007',
         'HOST': 'localhost',
@@ -141,3 +141,4 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    class Meta:
+        db_table = 'User'
+        verbose_name = 'Пользователя'
+        verbose_name_plural = 'Пользователя'
+
+    image =  models.ImageField(upload_to='users_images', verbose_name='Аватар', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
